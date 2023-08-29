a97b82bda3fb49c5a7429577fc6c61d52d8e1b0c
sf-shtirlitz <118398231+sf-shtirlitz@users.noreply.github.com>
2023-08-29 09:42:54 -0700

    OrderServiceApplication modified

--- order-service/src/main/java/com/vinsguru/order/OrderServiceApplication.java
+++ order-service/src/main/java/com/vinsguru/order/OrderServiceApplication.java
@@ -1,13 +1,19 @@
 package com.vinsguru.order;
 
+import com.vinsguru.order.application.service.InventoryComponentService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class OrderServiceApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(OrderServiceApplication.class, args);
+
+        ConfigurableApplicationContext context = SpringApplication.run(OrderServiceApplication.class, args);
+        //context.getBean(beanName);
+        //context.getBean(InventoryComponentService.class);
+        //examples of use above
     }
 
 }
